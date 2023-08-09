2708cac3796ff059970a21258c7c1c44c6d26562
Daniel Kelly <me@danielkelly.io>
2023-08-09 11:32:24 -0500

    4-watching-arrays-the-wrong-way

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import { useCounter } from './composables/useCounter'
-import MyComponent from '@/components/MyComponent.vue'
-const { count } = useCounter()
+import WatchArrayExample from '@/components/WatchArrayExample.vue'
 </script>
 <template>
-  <button @click="count++">count</button>
-  <MyComponent />
+  <WatchArrayExample />
 </template>
---
+++ src/components/WatchArrayExample.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+const numbers = ref<number[]>([])
+
+watch(numbers, () => console.log('new number added'), {
+  deep: true
+})
+
+const randomNum = () => numbers.value.push(Math.random())
+</script>
+<template>
+  <button @click="randomNum">Add Random Number</button>
+</template>
