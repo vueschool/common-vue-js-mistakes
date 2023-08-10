a11344b2953080d81ce15b1cc518e7c8c1337eef
Daniel Kelly <me@danielkelly.io>
2023-08-10 14:54:53 -0500

    9-not-considering-typescript

---
+++ src/components/TSMistake.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const user = ref({
+  username: 'danielkelly_io',
+  email: 'daniel@vueschool.io',
+  name: 'Daniel Kelly'
+})
+</script>
+
+<template>
+  <ul>
+    <li>
+      <strong>Username</strong>
+      <span>{{ user.username }}</span>
+    </li>
+    <li>
+      <strong>Email</strong>
+      <span>{{ user.email }}</span>
+    </li>
+    <li>
+      <strong>Name</strong>
+      <span>{{ user.name }}</span>
+    </li>
+  </ul>
+</template>
